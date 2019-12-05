79fee4f94edebbf5f82ffe96d6bbfcca88c0c658
maroKanatani <5genbassnohito.maroro0208@gmail.com>
2019-12-05 14:09:37 +0900

    unittestのサンプルと使用ライブラリ等「変更

--- app/src/main.py
+++ app/src/main.py
@@ -1,22 +1,17 @@
-import sqlalchemy as sa
 from flask import Flask
+from flask_restplus import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
- 
-@app.route('/')
-def hello():
-    url = 'mysql+pymysql://test:test@sample_db:3306/dc_db?charset=utf8'
-    engine = sa.create_engine(url, echo=True)
-    rows = engine.execute('SELECT * FROM users')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://test:test@sample_db:3306/dc_db?charset=utf8'
+db = SQLAlchemy(app)
+api = Api(app)
 
-    names = [("<h1>" + row['name'] + "</h1>" + "<h1>" + row['name'] + "</h1>" + "<h1>" + row['name'] + "</h1>") for row in rows]
-    response = ''.join(names)
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        
+        return {'hello': 'world'}
 
-    return response
-
-@app.route('/hello')
-def hello2():
-    return "Hello"
- 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
